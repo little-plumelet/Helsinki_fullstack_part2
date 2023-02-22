880649b58b5d69e418418ec5ffe31410085e4f66
Anastasiia Perova <perova@MacBook-Pro-Anastasiia.local>
2023-02-22 16:47:21 +0400

    finish task 2.6 - add phonebook page'

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
  } from 'react-router-dom';
  import React from 'react';
 import { AnecdotesPage } from './anecdotes/AnecdotesPage';
+import { PhonebookPage } from './phonebook/PhonebookPage';
  
 const App = () => {
 
@@ -18,12 +19,14 @@ const App = () => {
           <li style={{listStyleType: 'none'}}><Link to='/courseinfo'>courseinfo</Link></li>
           <li style={{listStyleType: 'none'}}><Link to='/unicafe'>unicafe</Link></li>
           <li style={{listStyleType: 'none'}}><Link to='/anecdotes'>anecdotes</Link></li>
+          <li style={{listStyleType: 'none'}}><Link to='/phonebook'>phonebook</Link></li>
         </ul>
       </div>
       <Routes>
         <Route exact path='/courseinfo' element={<CourseinfoPage />} />
         <Route exact path='/unicafe' element={<UniCafePage />} />
         <Route exact path='/anecdotes' element={<AnecdotesPage />} />
+        <Route exact path='/phonebook' element={<PhonebookPage />} />
       </Routes>
     </Router>
   )
---
+++ src/phonebook/PhonebookPage.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export const PhonebookPage = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const addNameHandler = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const submitNameHandler = (event) => {
+    event.preventDefault();
+    setPersons([...persons, {name: newName}]);
+    setNewName('');
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={addNameHandler} />
+        </div>
+        <div>
+          <button type="submit" onClick={submitNameHandler}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <div key={person.name}>{person.name}</div>)}
+    </div>
+  );
+};

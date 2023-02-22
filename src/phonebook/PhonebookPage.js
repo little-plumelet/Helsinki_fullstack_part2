54ed9e7ba2e70a0cebc23733418e1568990aa8ea
Anastasiia Perova <perova@MacBook-Pro-Anastasiia.local>
2023-02-22 19:34:08 +0400

    finish task 2.9 - add filter by name to phonebook

--- src/phonebook/PhonebookPage.js
+++ src/phonebook/PhonebookPage.js
@@ -1,9 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export const PhonebookPage = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", phone: '+8-888-777-66-55' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
+  ]);
+  const [filteredPersons, setFilteredPersons] = useState([...persons]);
   const [newName, setNewName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [filterName, setFilterName] = useState('');
+
+  const changeFilterNameHandler = (event) => {
+    const filter = event.target.value;
+    setFilterName(filter);
+    if (filter === '') {
+      setFilteredPersons([...persons])
+    } else {
+      setFilteredPersons(persons.reduce((acc, person) => {
+        if (person.name.toLowerCase().includes(filter.toLowerCase())) {
+          acc.push(person);
+        };
+        return acc;
+      }, []))
+    }
+  }
 
   const changeNameHandler = (event) => {
     setNewName(event.target.value);
@@ -20,23 +42,26 @@ export const PhonebookPage = () => {
       return;
     }
     if (!newName || !phoneNumber) {
-      alert('You must fill all fields!');
+      alert("You must fill all fields!");
       return;
     }
     if (!phoneNumber.match(/\+[0-9]{1,3}-[0-9]{3}-[0-9]{2}-[0-9]{2}/g)) {
-      alert('You phone mast be like +995-888-77-66!');
+      alert("You phone mast be like +995-888-77-66!");
       return;
     }
     setPersons([...persons, { name: newName, phone: phoneNumber }]);
-    setNewName('');
-    setPhoneNumber('');
+    setNewName("");
+    setPhoneNumber("");
   };
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input value={filterName} onChange={changeFilterNameHandler} />
+      </div>
       <form>
         <div>
-          name: <input value={newName} onChange={changeNameHandler}/>
+          name: <input value={newName} onChange={changeNameHandler} />
         </div>
         <div>
           number:{" "}
@@ -53,8 +78,10 @@ export const PhonebookPage = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>{person.name} {person.phone}</div>
+      {filteredPersons.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.phone}
+        </div>
       ))}
     </div>
   );

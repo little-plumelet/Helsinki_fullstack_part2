bc8fc97cc3cd7cde6b4088a76abe8727218712b8
Anastasiia Perova <perovaa.24@gmail.com>
2023-02-28 11:47:48 +0400

    change phonebook to table

--- src/phonebook/PhonebookList.js
+++ src/phonebook/PhonebookList.js
@@ -1,15 +1,29 @@
 import { Button } from "./Button";
 
-export const PhonebookList = ({filteredPersons, handleDeleteClick}) => {
+export const PhonebookList = ({ filteredPersons, handleDeleteClick }) => {
   return (
     <>
       <h2>Numbers</h2>
-      {filteredPersons.map((person) => (
-        <div key={person.id}>
-          {person.name} {person.phone}
-          <Button title='delete' handleClick={() => {handleDeleteClick(person.id)}}/>
-        </div>
-      ))}
+      <table>
+        <thead></thead>
+        <tbody>
+          {filteredPersons.map((person) => (
+            <tr key={person.id}>
+              <td>{person.name}</td>
+              <td> {person.phone}</td>
+              <td>
+                {" "}
+                <Button
+                  title="delete"
+                  handleClick={() => {
+                    handleDeleteClick(person.id);
+                  }}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
